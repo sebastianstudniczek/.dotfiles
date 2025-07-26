f4940935093f851f0b13912ccbfd68c01f27988e
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-07-26 23:02:10 +0200

    NeoVim - add roslyn diagnostic refresh

--- nvim/.config/nvim/lua/plugins/roslyn-nvim.lua
+++ nvim/.config/nvim/lua/plugins/roslyn-nvim.lua
@@ -1,3 +1,23 @@
+-- Refresh diagnostics
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  pattern = "*",
+  callback = function()
+    local clients = vim.lsp.get_clients({ name = "roslyn" })
+    if not clients or #clients == 0 then
+      return
+    end
+
+    local client = assert(clients[1])
+    local buffers = vim.lsp.get_buffers_by_client_id(client.id)
+    for _, buf in ipairs(buffers) do
+      local params = {
+        textDocument = vim.lsp.util.make_text_document_params(buf),
+      }
+      client:request(vim.lsp.protocol.Methods.textDocument_diagnostic, params, nil, buf)
+    end
+  end,
+})
+
 vim.lsp.config("roslyn", {
   settings = {
     ["csharp|inlay_hints"] = {
