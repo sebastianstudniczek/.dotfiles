6e601dd2d472ec952d60648ccb7d97f70023900c
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-07-26 22:53:44 +0200

    NeoVim - extract roslyn-nvim config

--- nvim/.config/nvim/lua/plugins/csharp.lua
+++ nvim/.config/nvim/lua/plugins/csharp.lua
@@ -1,37 +1,3 @@
-vim.lsp.config("roslyn", {
-  settings = {
-    ["csharp|inlay_hints"] = {
-      csharp_enable_inlay_hints_for_implicit_object_creation = true,
-      csharp_enable_inlay_hints_for_implicit_variable_types = true,
-      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-      csharp_enable_inlay_hints_for_types = true,
-      dotnet_enable_inlay_hints_for_indexer_parameters = true,
-      dotnet_enable_inlay_hints_for_literal_parameters = true,
-      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-      dotnet_enable_inlay_hints_for_other_parameters = true,
-      dotnet_enable_inlay_hints_for_parameters = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-    },
-    ["csharp|code_lens"] = {
-      dotnet_enable_references_code_lens = true,
-    },
-    ["csharp|completion"] = {
-      dotnet_show_completion_items_from_unimported_namespaces = true,
-      dotnet_show_name_completion_suggestions = true,
-      dotnet_provide_regex_completions = true,
-    },
-    ["csharp|background_analysis"] = {
-      dotnet_analyzer_diagnostics_scope = "fullSolution",
-      dotnet_compiler_diagnostics_scope = "fullSolution",
-    },
-    ["csharp|symbol_search"] = {
-      dotnet_search_reference_assemblies = true,
-    },
-  },
-})
-
 return {
   {
     "GustavEikaas/easy-dotnet.nvim",
@@ -97,16 +63,6 @@ return {
     },
   },
   {
-    "seblyng/roslyn.nvim",
-    ft = "cs",
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {
-      -- your configuration comes here; leave empty for default settings
-      -- NOTE: You must configure `cmd` in `config.cmd` unless you have installed via mason
-    },
-  },
-  {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
@@ -120,6 +76,7 @@ return {
       },
     },
   },
+  {},
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
