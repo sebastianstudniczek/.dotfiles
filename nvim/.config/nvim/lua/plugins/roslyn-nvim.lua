832f08c5a49a95574c3c41282073e60d2234b5ba
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-07-28 22:13:17 +0200

    NeoVim - extract roslyn config

--- nvim/.config/nvim/lua/plugins/roslyn-nvim.lua
+++ nvim/.config/nvim/lua/plugins/roslyn-nvim.lua
@@ -18,6 +18,105 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
   end,
 })
 
+-- Fix usings
+vim.api.nvim_create_user_command("CSFixUsings", function()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local clients = vim.lsp.get_clients({ name = "roslyn" })
+  if not clients or vim.tbl_isempty(clients) then
+    vim.notify("Couldn't find client", vim.log.levels.ERROR, { title = "Roslyn" })
+    return
+  end
+
+  local client = clients[1]
+  local action = {
+    kind = "quickfix",
+    data = {
+      CustomTags = { "RemoveUnnecessaryImports" },
+      TextDocument = { uri = vim.uri_from_bufnr(bufnr) },
+      CodeActionPath = { "Remove unnecessary usings" },
+      Range = {
+        ["start"] = { line = 0, character = 0 },
+        ["end"] = { line = 0, character = 0 },
+      },
+      UniqueIdentifier = "Remove unnecessary usings",
+    },
+  }
+
+  client:request("codeAction/resolve", action, function(err, resolved_action)
+    if err then
+      vim.notify(err)
+      vim.notify("Fix using directives failed", vim.log.levels.ERROR, { title = "Roslyn" })
+      return
+    end
+    vim.lsp.util.apply_workspace_edit(resolved_action.edit, client.offset_encoding)
+  end)
+end, { desc = "Remove unnecessary using directives" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cs",
+  callback = function(event)
+    vim.keymap.set("n", "<leader>coi", ":CSFixUsings<CR>", {
+      desc = "Remove unnecessary using directives",
+      buffer = event.buf,
+    })
+  end,
+})
+
+-- Auto expand (ie method comments)
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+
+    if client and (client.name == "roslyn" or client.name == "roslyn_ls") then
+      vim.api.nvim_create_autocmd("InsertCharPre", {
+        desc = "Roslyn: Trigger an auto insert on '/'.",
+        buffer = bufnr,
+        callback = function()
+          local char = vim.v.char
+
+          if char ~= "/" then
+            return
+          end
+
+          local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+          row, col = row - 1, col + 1
+          local uri = vim.uri_from_bufnr(bufnr)
+
+          local params = {
+            _vs_textDocument = { uri = uri },
+            _vs_position = { line = row, character = col },
+            _vs_ch = char,
+            _vs_options = {
+              tabSize = vim.bo[bufnr].tabstop,
+              insertSpaces = vim.bo[bufnr].expandtab,
+            },
+          }
+
+          -- NOTE: We should send textDocument/_vs_onAutoInsert request only after
+          -- buffer has changed.
+          vim.defer_fn(function()
+            client:request(
+              ---@diagnostic disable-next-line: param-type-mismatch
+              "textDocument/_vs_onAutoInsert",
+              params,
+              function(err, result, _)
+                if err or not result then
+                  return
+                end
+
+                vim.snippet.expand(result._vs_textEdit.newText)
+              end,
+              bufnr
+            )
+          end, 1)
+        end,
+      })
+    end
+  end,
+})
+
 vim.lsp.config("roslyn", {
   settings = {
     ["csharp|inlay_hints"] = {
