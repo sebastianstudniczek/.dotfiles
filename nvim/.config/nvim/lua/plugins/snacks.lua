b1a6b19463251e6830c1b12f84e8863af359a50a
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-08-26 09:00:51 +0200

    NeoVim - snacks move to trash on delete

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -35,6 +35,61 @@ return {
                   actions.update(picker, { target = item_path })
                 end)
               end,
+              -- Overide default behvaior so that deleted files are moved to trash instead of deleted permanently
+              explorer_del = function(picker) --[[Override]]
+                local actions = require("snacks.explorer.actions")
+                local Tree = require("snacks.explorer.tree")
+                local paths = vim.tbl_map(Snacks.picker.util.path, picker:selected({ fallback = true }))
+                if #paths == 0 then
+                  return
+                end
+                local what = #paths == 1 and vim.fn.fnamemodify(paths[1], ":p:~:.") or #paths .. " files"
+                actions.confirm("Put to the trash " .. what .. "?", function()
+                  for _, path in ipairs(paths) do
+                    local cmd
+                    local is_windows = vim.fn.has("win32") == 1
+
+                    if is_windows then
+                      local function_name
+                      if vim.loop.fs_stat(path).type == "directory" then
+                        function_name = "DeleteDirectory"
+                      else
+                        function_name = "DeleteFile"
+                      end
+
+                      cmd = {
+                        "powershell.exe",
+                        "-Command",
+                        string.format(
+                          "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.FileIO.FileSystem]::%s('%s', 'OnlyErrorDialogs', 'SendToRecycleBin')",
+                          function_name,
+                          path
+                        ),
+                      }
+                    else
+                      if vim.fn.executable("trash") == 1 then
+                        cmd = { "trash", path }
+                      else
+                        Snacks.notify.error("Missing `trash` CLI on this system")
+                        return
+                      end
+                    end
+
+                    local ok, result = pcall(function()
+                      return vim.system(cmd, { text = true })
+                    end)
+
+                    if ok then
+                      Snacks.bufdelete({ file = path, force = true })
+                    else
+                      Snacks.notify.error("Failed to delete `" .. path .. "`:\n- " .. result)
+                    end
+                    Tree:refresh(vim.fs.dirname(path))
+                  end
+                  picker.list:set_selected()
+                  actions.update(picker)
+                end)
+              end,
             },
           },
         },
