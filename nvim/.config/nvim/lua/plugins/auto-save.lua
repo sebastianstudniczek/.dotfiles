f7967ded1098971df06917dbe2925b0398da9152
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-08-27 17:49:27 +0200

    NeoVim - update auto-save config

--- nvim/.config/nvim/lua/plugins/auto-save.lua
+++ nvim/.config/nvim/lua/plugins/auto-save.lua
@@ -2,11 +2,19 @@ local included_filetypes = {
   "cs",
 }
 
+local included_extensions = {
+  "csproj",
+}
+
 return {
   "okuuva/auto-save.nvim",
+  lazy = false,
   opts = {
     condition = function(buf)
-      if vim.tbl_contains(included_filetypes, vim.fn.getbufvar(buf, "&filetype")) then
+      if
+        vim.tbl_contains(included_filetypes, vim.fn.getbufvar(buf, "&filetype"))
+        or vim.tbl_contains(included_extensions, vim.fn.expand("%:e"))
+      then
         return true
       end
 
