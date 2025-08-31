07f6a2e42b3e866613d080a76f60787b4b2bf8a4
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-08-31 22:50:34 +0200

    NeoVim - disable blink ai completions

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable ai completions in autosuggestions popup
+vim.g.ai_cmp = false
+
 -- Setup pwsh for command line actions
 if vim.fn.has("win32") == 1 then
   vim.opt.shell = "pwsh"
