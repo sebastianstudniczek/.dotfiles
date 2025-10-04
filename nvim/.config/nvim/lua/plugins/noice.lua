2011a6976e7055016627d609e9a66a706bababc5
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-10-04 22:53:19 +0200

    NeoVim - noice discard roslyn lsp progress

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -2,14 +2,20 @@ return {
   {
     "folke/noice.nvim",
     opts = {
+      -- TODO: Disabling LSP proegress until this is fixed for roslyn.nvim https://github.com/seblyng/roslyn.nvim/issues/236
+      routes = {
+        filter = {
+          event = "lsp",
+          kind = "progress",
+          cond = function(message)
+            local client = vim.tbl_get(message.opts, "progress", "client")
+            return client == "roslyn"
+          end,
+        },
+        opts = { skip = true },
+      },
       presets = {
         lsp_doc_border = true,
-      },
-      -- TODO: Disabling LSP proegress until this is fixed for roslyn.nvim https://github.com/seblyng/roslyn.nvim/issues/236
-      lsp = {
-        progress = {
-          enabled = false,
-        },
       },
       notify = {
         view = "mini",
