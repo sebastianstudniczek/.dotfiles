37943efb5c9addd039e4c77fd83978fc9ac1a854
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2025-10-21 23:11:31 +0200

    NeoVim - blink simulare rider lsp completion order

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,8 +1,50 @@
+local kind = require("blink.cmp.types").CompletionItemKind
+
+local order = {
+  [kind.Field] = 1,
+  [kind.Property] = 2,
+  [kind.Method] = 3,
+  [kind.Operator] = 4,
+}
+
 return {
   -- Use mini icons in completions
   {
     "saghen/blink.cmp",
     opts = {
+      fuzzy = {
+        sorts = {
+          "exact",
+          "score",
+          -- Simulate sorting order similar like in Rider
+          -- TODO: Waiting for this to be implemented https://github.com/saghen/blink.cmp/issues/2073
+          function(a, b)
+            if a.client_name ~= "roslyn" and b.client_name ~= "roslyn" then
+              return
+            end
+
+            local a_order = order[a.kind]
+            local b_order = order[b.kind]
+
+            if a_order == nil and b_order == nil then
+              return
+            end
+
+            if a_order == nil and b_order ~= nil then
+              return false
+            end
+
+            if a_order ~= nil and b_order == nil then
+              return true
+            end
+
+            return a_order < b_order
+          end,
+          "sort_text",
+          "kind",
+          "label",
+        },
+      },
       completion = {
         menu = {
           draw = {
