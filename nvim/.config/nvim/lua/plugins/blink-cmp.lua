49ce0c50e816f79a48231e21d15083dfd1547043
Sebastian Studniczek <sebastian.studniczek@northmill.se>
2026-01-23 19:06:07 +0100

    NeoVim - Blink enable nuget completion for easy-dotnet

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -12,6 +12,23 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
+      sources = {
+        default = {
+          "lsp",
+          "easy-dotnet",
+          "path",
+        },
+        providers = {
+          ["easy-dotnet"] = {
+            name = "easy-dotnet",
+
+            enabled = true,
+            module = "easy-dotnet.completion.blink",
+            score_offset = 10000,
+            async = true,
+          },
+        },
+      },
       fuzzy = {
         sorts = {
           "exact",
