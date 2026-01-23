c7e06db7de5b7d5c93d925c97b7f7d6a391806e4
Sebastian Studniczek <sebastian.studniczek@northmill.se>
2026-01-23 19:02:20 +0100

    NeoVim - disable WSL clipboard integration
    Pasting is slow, verify later

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -17,18 +17,3 @@ if vim.fn.has("win32") == 1 then
   vim.opt.shellquote = ""
   vim.opt.shellxquote = ""
 end
-
-if vim.fn.has("wsl") == 1 then
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "/mnt/c/Windows/System32/clip.exe",
-      ["*"] = "/mnt/c/Windows/System32/clip.exe",
-    },
-    paste = {
-      ["+"] = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
-end
