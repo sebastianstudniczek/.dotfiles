797f677d6e5ee952324f3caecabcc4a42808ce77
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-02-09 22:59:46 +0100

    Neovim - set clipboard keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Copy/paste with system clipboard
+vim.keymap.set({ "n", "x" }, "gy", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set("n", "gp", '"+p', { desc = "Paste from system clipboard" })
+-- - Paste in Visual with `P` to not copy selected text (`:h v_P`)
+vim.keymap.set("x", "gp", '"+P', { desc = "Paste from system clipboard" })
