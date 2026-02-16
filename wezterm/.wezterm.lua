fa69291034e8f1a29bd2299d3a67abd0cc435496
Sebastian Studniczek <sebastian.studniczek@northmill.se>
2026-02-16 20:10:03 +0100

    Wezterm - improve sessionizer

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,24 +5,28 @@ local mux = wezterm.mux
 
 local keys = {}
 
-local gpus = wezterm.gui.enumerate_gpus()
-
 config = {
-	-- webgpu_preferred_adapter = gpus[3],
+	front_end = "WebGpu",
 	adjust_window_size_when_changing_font_size = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
-	font_size = 9,
-	front_end = "WebGpu",
+	font_size = 10,
 	color_scheme = "Tokyo Night",
-	font = wezterm.font("JetBrains Mono"),
+	font = wezterm.font("Iosevka Nerd Font"),
 	hide_tab_bar_if_only_one_tab = false,
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = true,
 	tab_max_width = 500,
-	win32_system_backdrop = "Mica",
-	window_background_opacity = 0.0,
+	default_cursor_style = "SteadyBar",
+	max_fps = 120,
 }
+
+for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
+	if gpu.backend == "Dx12" and gpu.device_type == "DiscreteGpu" then
+		config.webgpu_preferred_adapter = gpu
+		break
+	end
+end
 
 -- Match Mica effect
 config.window_frame = {
@@ -31,6 +35,7 @@ config.window_frame = {
 
 -- FG color to match rose pine theme
 config.colors = {
+	background = "#191A1C",
 	tab_bar = {
 		inactive_tab = {
 			bg_color = "rgba(0, 0, 0, 0.26)",
@@ -54,6 +59,7 @@ end)
 
 local sessionizer = wezterm.plugin.require("https://github.com/mikkasendke/sessionizer.wezterm")
 local sessionizer_zoxide = wezterm.plugin.require("https://github.com/mikkasendke/sessionizer-zoxide")
+local history = wezterm.plugin.require("https://github.com/mikkasendke/sessionizer-history")
 
 local PredefinedEntries = function()
 	return function()
@@ -66,6 +72,7 @@ end
 local sessionizer_schema = {
 	options = {
 		prompt = "Workspace to switch: ",
+		callback = history.Wrapper(sessionizer.DefaultCallback),
 	},
 	{
 		sessionizer.AllActiveWorkspaces({ filter_current = false, filter_default = false }),
@@ -94,10 +101,13 @@ local sessionizer_schema = {
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 -- Workaround for blink completion, for some reason it does not work properly on windows
 table.insert(keys, { key = " ", mods = "CTRL", action = wezterm.action.SendKey({ key = " ", mods = "CTRL" }) })
+-- HACK: By default terminal is not recognizing this key combination, so we need to send it manually
+table.insert(keys, { key = ".", mods = "CTRL", action = wezterm.action.SendKey({ key = ".", mods = "CTRL" }) })
 
 table.insert(keys, { key = "k", mods = "LEADER", action = sessionizer.show(sessionizer_schema) })
-table.insert(keys, { key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") })
-table.insert(keys, { key = "w", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) })
+table.insert(keys, { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") })
+table.insert(keys, { key = "x", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) })
+table.insert(keys, { key = "L", mods = "LEADER", action = history.switch_to_most_recent_workspace })
 
 for i = 1, 9 do
 	table.insert(keys, {
