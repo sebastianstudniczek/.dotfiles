661a41c0f7b24947a60edf33f7d09e1d23ac7206
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-02-25 21:46:04 +0100

    NeoVim - add allman brace style snippet for C#

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,24 @@ vim.keymap.set({ "n", "x" }, "gy", '"+y', { desc = "Copy to system clipboard" })
 vim.keymap.set("n", "gp", '"+p', { desc = "Paste from system clipboard" })
 -- - Paste in Visual with `P` to not copy selected text (`:h v_P`)
 vim.keymap.set("x", "gp", '"+P', { desc = "Paste from system clipboard" })
+
+--- Allman style braces for C#
+local function allman_brace()
+  local line = vim.fn.getline(".")
+  local col = vim.fn.col(".") - 1
+  local before = line:sub(1, col)
+
+  local should_expand = before:match("%)%s*$") or before:match("%{%s*$")
+  if not should_expand then
+    return "{"
+  end
+
+  return "<CR>{<CR>}<Esc>O"
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cs" },
+  callback = function()
+    vim.keymap.set("i", "{", allman_brace, { expr = true, buffer = true })
+  end,
+})
