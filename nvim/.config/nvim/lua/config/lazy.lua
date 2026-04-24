47e8478e76d5b7c040405d4b0dfd0544327cfcfc
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-04-25 00:35:59 +0200

    NeoVim - disable tracking lazyvim.json
    Some extras should be enabled locally only on specific
    machines via LazyUi

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -34,6 +34,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.util.gh" },
 
     -- import/override with your plugins
     { import = "plugins" },
