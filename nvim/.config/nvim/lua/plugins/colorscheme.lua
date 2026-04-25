7d8bf46de2575919cd284f25c45815e2b9831e64
Sebastian Studniczek <sebastian.studniczek@northmill.se>
2026-04-25 17:27:02 +0200

    NeoVim - add rose-pine theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,24 @@
 return {
   {
+    "rose-pine/neovim",
+    enabled = true,
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          transparency = true,
+          italic = false,
+        },
+        highlight_groups = {
+          NormalFloat = { bg = "NONE" },
+          BlinkCmpDoc = { bg = "NONE" },
+          BlinkCmpLabel = { fg = "foam" },
+        },
+      })
+      vim.cmd("colorscheme rose-pine-moon")
+    end,
+  },
+  {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
