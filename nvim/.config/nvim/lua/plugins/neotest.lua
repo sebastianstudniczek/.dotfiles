5f890f2ce45b6f010445bd4d221da62362bea856
Sebastian Studniczek <sebastian.studniczek@northmill.se>
2026-04-25 17:53:59 +0200

    NeoVim - neotest add log highlight for test output

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,4 +1,23 @@
 local handle = nil
+
+-- Add syntax highlighting for neotest output using log-highlight.nvim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "neotest-output",
+  callback = function()
+    -- relies on log-highlight.nvim being installed
+    -- This tells Neovim to use the syntax rules defined for .log files
+    -- while keeping the 'neotest-output' filetype for functionality.
+    vim.bo.syntax = "log"
+
+    -- HACK: Looks like neotest is setting options after buffer is created
+    vim.defer_fn(function()
+      if vim.api.nvim_buf_is_valid(0) then
+        vim.bo.syntax = "log"
+      end
+    end, 10)
+  end,
+})
+
 return {
   {
     "nvim-neotest/neotest",
