266726a0da49189e8f6a4a5c27a000c7fbbc2009
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-05-15 19:24:08 +0200

    NeoVim - cleanup

--- nvim/.config/nvim/lua/plugins/custom.lua
+++ nvim/.config/nvim/lua/plugins/custom.lua
@@ -5,14 +5,6 @@ return {
     opts = {},
   },
   {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        yaml = { "cfn_lint" },
-      },
-    },
-  },
-  {
     "folke/flash.nvim",
     opts = {
       modes = {
