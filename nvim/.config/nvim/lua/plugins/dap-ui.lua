2ff3677696985ef6b395718aad97c28bd915882c
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2026-06-07 23:05:42 +0200

    NeoVim - dap switch to nvim-dap-view
    Simplified interface - single buffer with multiple tabs

--- nvim/.config/nvim/lua/plugins/dap-ui.lua
+++ nvim/.config/nvim/lua/plugins/dap-ui.lua
@@ -1,19 +1,30 @@
 return {
-  "rcarriga/nvim-dap-ui",
-  opts = {
-    expand_lines = true,
-    controls = { enabled = false },
-    floating = { border = "rounded" },
+  {
+    "rcarriga/nvim-dap-ui",
+    enabled = false,
+    opts = {
+      expand_lines = true,
+      controls = { enabled = false },
+      floating = { border = "rounded" },
 
-    layouts = {
-      {
-        elements = {
-          { id = "scopes", size = 0.5 },
-          { id = "repl", size = 0.5 },
+      layouts = {
+        {
+          elements = {
+            { id = "scopes", size = 0.5 },
+            { id = "repl", size = 0.5 },
+          },
+          size = 15,
+          position = "bottom",
         },
-        size = 15,
-        position = "bottom",
       },
+    },
+  },
+  {
+    "igorlfs/nvim-dap-view",
+    lazy = false,
+    version = "1.*",
+    opts = {
+      auto_toggle = true,
     },
   },
 }
